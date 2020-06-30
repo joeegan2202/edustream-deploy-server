b6a2d8c71f6c6869bca3c9a847bf0efe1b8dc9d5
Joe Egan <joeegan2202@gmail.com>
2020-06-30 15:09:59 -0400

    Added loading private key and starting to add signing stream data

--- server.go
+++ server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/sha256"
+  "crypto/x509"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-  "strings"
-  "os/exec"
-  "os"
-  "fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/gorilla/mux"
-  "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
+
+var key *rsa.PrivateKey
 
 type Feed struct {
   address string
@@ -27,10 +34,29 @@ func main() {
 
   port := os.Getenv("PORT")
 
+  setupKey()
+
   r := mux.NewRouter()
 
   r.HandleFunc("/add/", addFeed)
+  r.HandleFunc("/ingest/", signStream)
   log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+}
+
+func setupKey() {
+  file, err := os.OpenFile("key.pem", os.O_RDWR, 0755)
+
+  if err != nil {
+    log.Fatalf("Couldn't open keyfile! %s\n", err.Error())
+  }
+
+  data, err := ioutil.ReadAll(file)
+
+  if err != nil {
+    log.Fatalf("Couldn't read from keyfile! %s\n", err.Error())
+  }
+
+  key, err = x509.ParsePKCS1PrivateKey(data)
 }
 
 func addFeed(w http.ResponseWriter, r *http.Request) {
@@ -76,4 +102,18 @@ func addFeed(w http.ResponseWriter, r *http.Request) {
 
   w.WriteHeader(http.StatusOK)
   w.Write([]byte("true;"))
+}
+
+func signStream(w http.ResponseWriter, r *http.Request) {
+  chunk := make([]byte, 2048)
+  _, err := io.ReadFull(r.Body, chunk)
+
+  if err != nil {
+    fmt.Printf("Error while trying to read chunk of body data! %s\n", err.Error())
+    return
+  }
+
+  hasher := sha256.New()
+
+  hasher.Write(chunk)
 }
